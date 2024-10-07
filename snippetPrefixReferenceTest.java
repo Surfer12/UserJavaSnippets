4cfd4e88afb857d25c63dc917f14c6a9e06d6b0f
Surfer12 <ryanoatsie@outlook.com>
2024-10-07 05:24:53 -0700

    refactor: Remove snippetPrefixReferenceTest class

--- snippetPrefixReferenceTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Stack;
-
-public class snippetPrefixReferenceTest {
-    private int[] nums;
-
-    public snippetPrefixReferenceTest(int[] nums) {
-        this.nums = nums;
-    }
-}
